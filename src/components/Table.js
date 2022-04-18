fe990db935e740250f00ef8fa1678c5b8c0d9ead
Jacqueline Silva <jacque.sx@hotmail.com>
2022-04-18 15:26:51 -0300

    Requisito 01 + iniciando Requisito 02

--- src/components/Table.js
+++ src/components/Table.js
@@ -4,40 +4,58 @@ import PlanetsContext from '../context/PlanetsContext';
 function Table() {
   const data = useContext(PlanetsContext);
 
+  function dataFilterKeys(dados) {
+    return Object.keys(dados[0]).filter((chave) => chave !== 'residents');
+  }
+
+  function dataFilterValues(planet) {
+    return Object.values(planet).filter((value) => value !== 'residents');
+  }
+
   return (
-    data.length === 0
-      ? 'Loading'
-      : (
-        <table>
-          <thead>
-            <tr>
-              {
-                Object.keys(data[0])
-                  .filter((chave) => chave !== 'residents')
-                  .map((e) => (
+    <>
+      <h1>StarWars</h1>
+      <div>
+        <h2>Filters</h2>
+        <label htmlFor="name">
+          Name:
+          <input
+            type="text"
+            id="name"
+            onChange={ () => {} }
+          />
+        </label>
+      </div>
+
+      { data.length === 0
+        ? 'Loading'
+        : (
+          <table border="1px">
+            <thead>
+              <tr>
+                {
+                  dataFilterKeys(data).map((e) => (
                     <th key={ e }>{ e }</th>
                   ))
-              }
-            </tr>
-          </thead>
-
-          <tbody>
-            {
-              data.map((planet) => (
-                <tr key={ planet.name }>
-                  {
-                    Object.values(planet)
-                      .filter((chave) => chave !== 'residents')
-                      .map((e) => (
+                }
+              </tr>
+            </thead>
+            <tbody>
+              {
+                data.map((planet) => (
+                  <tr key={ planet.name }>
+                    {
+                      dataFilterValues(planet).map((e) => (
                         <td key={ e }>{ e }</td>
                       ))
-                  }
-                </tr>
-              ))
-            }
-          </tbody>
-        </table>
-      )
+                    }
+                  </tr>
+                ))
+              }
+            </tbody>
+          </table>
+        )}
+    </>
   );
 }
 
