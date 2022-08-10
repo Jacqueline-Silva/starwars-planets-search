d648186315c0d7cc03d30d81bf4c7ee7fd70ddec
Jacqueline Silva <jacque.sx@hotmail.com>
2022-08-10 11:15:14 -0300

    Updates

---
+++ precache-manifest.90373f430e83c5e7a9054935066ad8c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21dfe592b9564c33f1092d56e2e37df2",
+    "url": "/starwars-planets-search/index.html"
+  },
+  {
+    "revision": "4f26ddacf5fb92d06fbf",
+    "url": "/starwars-planets-search/static/css/main.2a90d803.chunk.css"
+  },
+  {
+    "revision": "201fde2ab43c6dc8e253",
+    "url": "/starwars-planets-search/static/js/2.dd68e627.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/starwars-planets-search/static/js/2.dd68e627.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f26ddacf5fb92d06fbf",
+    "url": "/starwars-planets-search/static/js/main.72b35391.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/starwars-planets-search/static/js/main.72b35391.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40188bf8d5b221e9fc28",
+    "url": "/starwars-planets-search/static/js/runtime-main.5733e3cb.js"
+  }
+]);
